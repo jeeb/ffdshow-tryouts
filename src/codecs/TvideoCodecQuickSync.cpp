jeeb/ffdshow-tryouts

commit f1265cbbd623927a3b388ddd7f4e3fc5035684ff

Sync with IntelQuickSync Decoder 0.29 (rev44) -Support for VFW -Improved performance under EVR work flow -Various MT related bug fixes.
FFDshow code changes:
-Removed sse_memcpy - was hurting performance under real world flows. Using the efficient memcpy instead. sse_memcpy was faster under GraphStudio which is not representing real world performance.
-Changed QS decoder proxy code to support VFW.




git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@4364 3b938f2f-1a1a-0410-8054-a526ea5ff92c

src/codecs/IntelQuickSyncDecoder/IntelQuickSyncDecoder.vcxproj
@@ -29,20 +29,24 @@
     <ConfigurationType>DynamicLibrary</ConfigurationType>
     <UseDebugLibraries>true</UseDebugLibraries>
     <CharacterSet>Unicode</CharacterSet>
+    <WholeProgramOptimization>false</WholeProgramOptimization>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'" Label="Configuration">
     <ConfigurationType>DynamicLibrary</ConfigurationType>
     <UseDebugLibraries>true</UseDebugLibraries>
     <CharacterSet>Unicode</CharacterSet>
+    <WholeProgramOptimization>false</WholeProgramOptimization>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'" Label="Configuration">
     <ConfigurationType>DynamicLibrary</ConfigurationType>
     <UseDebugLibraries>false</UseDebugLibraries>
+    <WholeProgramOptimization>false</WholeProgramOptimization>
     <CharacterSet>Unicode</CharacterSet>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'" Label="Configuration">
     <ConfigurationType>DynamicLibrary</ConfigurationType>
     <UseDebugLibraries>false</UseDebugLibraries>
+    <WholeProgramOptimization>false</WholeProgramOptimization>
     <CharacterSet>Unicode</CharacterSet>
   </PropertyGroup>
   <Import Project="$(VCTargetsPath)\Microsoft.Cpp.props" />
@@ -50,106 +54,189 @@
   </ImportGroup>
   <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
     <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
-    <Import Project="..\..\..\common.props" />
   </ImportGroup>
   <ImportGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'" Label="PropertySheets">
     <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
-    <Import Project="..\..\..\common.props" />
   </ImportGroup>
   <ImportGroup Label="PropertySheets" Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
     <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
-    <Import Project="..\..\..\common.props" />
   </ImportGroup>
   <ImportGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'" Label="PropertySheets">
     <Import Project="$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props" Condition="exists('$(UserRootDir)\Microsoft.Cpp.$(Platform).user.props')" Label="LocalAppDataPlatform" />
-    <Import Project="..\..\..\common.props" />
   </ImportGroup>
   <PropertyGroup Label="UserMacros" />
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
+    <LinkIncremental>true</LinkIncremental>
+    <OutDir>$(SolutionDir)obj\$(Configuration)_$(PlatformName)_VC2010\$(ProjectName)\</OutDir>
+    <IntDir>$(SolutionDir)obj\$(Configuration)_$(PlatformName)_VC2010\$(ProjectName)\</IntDir>
+    <GenerateManifest>true</GenerateManifest>
     <IncludePath>src\MSDK\include;$(IncludePath)</IncludePath>
     <LibraryPath>src\MSDK\lib\$(PlatformName);$(LibraryPath)</LibraryPath>
+    <IgnoreImportLibrary>false</IgnoreImportLibrary>
+    <CodeAnalysisRuleSet>AllRules.ruleset</CodeAnalysisRuleSet>
+    <RunCodeAnalysis>false</RunCodeAnalysis>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">
+    <LinkIncremental>true</LinkIncremental>
+    <OutDir>$(SolutionDir)obj\$(Configuration)_$(PlatformName)_VC2010\$(ProjectName)\</OutDir>
+    <IntDir>$(SolutionDir)obj\$(Configuration)_$(PlatformName)_VC2010\$(ProjectName)\</IntDir>
+    <GenerateManifest>true</GenerateManifest>
     <IncludePath>src\MSDK\include;$(IncludePath)</IncludePath>
     <LibraryPath>src\MSDK\lib\$(PlatformName);$(LibraryPath)</LibraryPath>
+    <IgnoreImportLibrary>false</IgnoreImportLibrary>
+    <CodeAnalysisRuleSet>AllRules.ruleset</CodeAnalysisRuleSet>
+    <RunCodeAnalysis>false</RunCodeAnalysis>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
+    <LinkIncremental>false</LinkIncremental>
+    <OutDir>$(SolutionDir)obj\$(Configuration)_$(PlatformName)_VC2010\</OutDir>
+    <IntDir>$(SolutionDir)obj\$(Configuration)_$(PlatformName)_VC2010\$(ProjectName)\</IntDir>
+    <GenerateManifest>true</GenerateManifest>
     <IncludePath>src\MSDK\include;$(IncludePath)</IncludePath>
     <LibraryPath>src\MSDK\lib\$(PlatformName);$(LibraryPath)</LibraryPath>
+    <IgnoreImportLibrary>false</IgnoreImportLibrary>
+    <CodeAnalysisRuleSet>AllRules.ruleset</CodeAnalysisRuleSet>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'">
+    <LinkIncremental>false</LinkIncremental>
+    <OutDir>$(SolutionDir)obj\$(Configuration)_$(PlatformName)_VC2010\</OutDir>
+    <IntDir>$(SolutionDir)obj\$(Configuration)_$(PlatformName)_VC2010\$(ProjectName)\</IntDir>
+    <GenerateManifest>true</GenerateManifest>
     <IncludePath>src\MSDK\include;$(IncludePath)</IncludePath>
     <LibraryPath>src\MSDK\lib\$(PlatformName);$(LibraryPath)</LibraryPath>
+    <IgnoreImportLibrary>false</IgnoreImportLibrary>
+    <CodeAnalysisRuleSet>AllRules.ruleset</CodeAnalysisRuleSet>
   </PropertyGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'">
     <ClCompile>
       <PrecompiledHeader>Use</PrecompiledHeader>
       <WarningLevel>Level4</WarningLevel>
+      <Optimization>Disabled</Optimization>
       <PreprocessorDefinitions>WIN32;DEBUG;_DEBUG;_WINDOWS;_USRDLL;QUICKSYNC_EXPORTS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <DisableSpecificWarnings>4996;4995</DisableSpecificWarnings>
-      <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
       <MinimalRebuild>false</MinimalRebuild>
+      <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
+      <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <EnablePREfast>false</EnablePREfast>
+      <TreatWarningAsError>false</TreatWarningAsError>
+      <MultiProcessorCompilation>true</MultiProcessorCompilation>
     </ClCompile>
     <Link>
       <SubSystem>Windows</SubSystem>
-      <ModuleDefinitionFile>src\IntelQuickSyncDecoder.def</ModuleDefinitionFile>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <ModuleDefinitionFile>src/IntelQuickSyncDecoder.def</ModuleDefinitionFile>
+      <IgnoreAllDefaultLibraries>false</IgnoreAllDefaultLibraries>
+      <OutputFile>$(SolutionDir)bin\$(TargetName)$(TargetExt)</OutputFile>
+      <ProgramDatabaseFile>$(OutDir)$(TargetName).pdb</ProgramDatabaseFile>
       <AdditionalDependencies>winmm.lib;uuid.lib;D3D9.lib;Dxva2.lib;libmfx.lib;strmiids.lib</AdditionalDependencies>
-      <IgnoreSpecificDefaultLibraries>LIBCMT.lib</IgnoreSpecificDefaultLibraries>
+      <Version>
+      </Version>
+      <IgnoreSpecificDefaultLibraries>LIBCMT.LIB</IgnoreSpecificDefaultLibraries>
+      <AdditionalOptions>/ignore:4099 %(AdditionalOptions)</AdditionalOptions>
     </Link>
-    <ResourceCompile>
-      <Culture>0x0409</Culture>
-    </ResourceCompile>
   </ItemDefinitionGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'">
     <ClCompile>
       <PrecompiledHeader>Use</PrecompiledHeader>
       <WarningLevel>Level4</WarningLevel>
+      <Optimization>Disabled</Optimization>
       <PreprocessorDefinitions>WIN32;DEBUG;_DEBUG;_WINDOWS;_USRDLL;QUICKSYNC_EXPORTS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <DisableSpecificWarnings>4996;4995</DisableSpecificWarnings>
-      <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
       <MinimalRebuild>false</MinimalRebuild>
+      <DebugInformationFormat>ProgramDatabase</DebugInformationFormat>
+      <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <EnablePREfast>false</EnablePREfast>
+      <TreatWarningAsError>true</TreatWarningAsError>
     </ClCompile>
     <Link>
       <SubSystem>Windows</SubSystem>
-      <ModuleDefinitionFile>src\IntelQuickSyncDecoder.def</ModuleDefinitionFile>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <ModuleDefinitionFile>src/IntelQuickSyncDecoder.def</ModuleDefinitionFile>
+      <IgnoreAllDefaultLibraries>false</IgnoreAllDefaultLibraries>
+      <OutputFile>$(SolutionDir)bin\$(TargetName)$(TargetExt)</OutputFile>
+      <ProgramDatabaseFile>$(OutDir)$(TargetName).pdb</ProgramDatabaseFile>
       <AdditionalDependencies>winmm.lib;uuid.lib;D3D9.lib;Dxva2.lib;libmfx.lib;strmiids.lib</AdditionalDependencies>
-      <IgnoreSpecificDefaultLibraries>LIBCMT.lib</IgnoreSpecificDefaultLibraries>
+      <Version>
+      </Version>
+      <IgnoreSpecificDefaultLibraries>LIBCMT.LIB</IgnoreSpecificDefaultLibraries>
+      <AdditionalOptions>/ignore:4099 %(AdditionalOptions)</AdditionalOptions>
     </Link>
-    <ResourceCompile>
-      <Culture>0x0409</Culture>
-    </ResourceCompile>
   </ItemDefinitionGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">
     <ClCompile>
-      <PrecompiledHeader>Use</PrecompiledHeader>
-      <PreprocessorDefinitions>WIN32;NDEBUG;_WINDOWS;_USRDLL;QUICKSYNC_EXPORTS;_CRT_SECURE_NO_WARNINGS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
-      <DisableSpecificWarnings>4996;4995</DisableSpecificWarnings>
       <WarningLevel>Level4</WarningLevel>
+      <PrecompiledHeader>Use</PrecompiledHeader>
+      <Optimization>Full</Optimization>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <IntrinsicFunctions>true</IntrinsicFunctions>
+      <PreprocessorDefinitions>WIN32;NDEBUG;_WINDOWS;_USRDLL;QUICKSYNC_EXPORTS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <DisableSpecificWarnings>4996;4995</DisableSpecificWarnings>
+      <MinimalRebuild>false</MinimalRebuild>
+      <RuntimeLibrary>MultiThreaded</RuntimeLibrary>
+      <BufferSecurityCheck>false</BufferSecurityCheck>
+      <StringPooling>true</StringPooling>
+      <EnablePREfast>false</EnablePREfast>
+      <TreatWarningAsError>true</TreatWarningAsError>
+      <FavorSizeOrSpeed>Speed</FavorSizeOrSpeed>
+      <OmitFramePointers>true</OmitFramePointers>
+      <EnableEnhancedInstructionSet>StreamingSIMDExtensions2</EnableEnhancedInstructionSet>
+      <WholeProgramOptimization>true</WholeProgramOptimization>
+      <MultiProcessorCompilation>true</MultiProcessorCompilation>
     </ClCompile>
     <Link>
       <SubSystem>Windows</SubSystem>
-      <ModuleDefinitionFile>src\IntelQuickSyncDecoder.def</ModuleDefinitionFile>
+      <EnableCOMDATFolding>true</EnableCOMDATFolding>
+      <OptimizeReferences>true</OptimizeReferences>
+      <ModuleDefinitionFile>src/IntelQuickSyncDecoder.def</ModuleDefinitionFile>
+      <IgnoreAllDefaultLibraries>false</IgnoreAllDefaultLibraries>
+      <OutputFile>$(SolutionDir)bin\$(TargetName)$(TargetExt)</OutputFile>
+      <ProgramDatabaseFile>$(OutDir)$(TargetName).pdb</ProgramDatabaseFile>
       <AdditionalDependencies>winmm.lib;uuid.lib;D3D9.lib;Dxva2.lib;libmfx.lib;strmiids.lib</AdditionalDependencies>
+      <Version>
+      </Version>
+      <IgnoreSpecificDefaultLibraries>LIBCMTD.LIB</IgnoreSpecificDefaultLibraries>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <LinkTimeCodeGeneration>UseLinkTimeCodeGeneration</LinkTimeCodeGeneration>
     </Link>
-    <ResourceCompile>
-      <Culture>0x0409</Culture>
-    </ResourceCompile>
   </ItemDefinitionGroup>
   <ItemDefinitionGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'">
     <ClCompile>
-      <PrecompiledHeader>Use</PrecompiledHeader>
-      <PreprocessorDefinitions>WIN32;NDEBUG;_WINDOWS;_USRDLL;QUICKSYNC_EXPORTS;_CRT_SECURE_NO_WARNINGS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
-      <DisableSpecificWarnings>4996;4995</DisableSpecificWarnings>
       <WarningLevel>Level4</WarningLevel>
+      <PrecompiledHeader>Use</PrecompiledHeader>
+      <Optimization>Full</Optimization>
+      <FunctionLevelLinking>true</FunctionLevelLinking>
+      <IntrinsicFunctions>true</IntrinsicFunctions>
+      <PreprocessorDefinitions>WIN32;NDEBUG;_WINDOWS;_USRDLL;QUICKSYNC_EXPORTS;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <DisableSpecificWarnings>4996;4995</DisableSpecificWarnings>
+      <MinimalRebuild>false</MinimalRebuild>
+      <RuntimeLibrary>MultiThreaded</RuntimeLibrary>
+      <BufferSecurityCheck>false</BufferSecurityCheck>
+      <StringPooling>true</StringPooling>
+      <EnablePREfast>false</EnablePREfast>
+      <FavorSizeOrSpeed>Speed</FavorSizeOrSpeed>
+      <AssemblerOutput>NoListing</AssemblerOutput>
+      <TreatWarningAsError>true</TreatWarningAsError>
+      <OmitFramePointers>true</OmitFramePointers>
+      <EnableEnhancedInstructionSet>StreamingSIMDExtensions2</EnableEnhancedInstructionSet>
+      <WholeProgramOptimization>true</WholeProgramOptimization>
     </ClCompile>
     <Link>
       <SubSystem>Windows</SubSystem>
-      <ModuleDefinitionFile>src\IntelQuickSyncDecoder.def</ModuleDefinitionFile>
+      <EnableCOMDATFolding>true</EnableCOMDATFolding>
+      <OptimizeReferences>true</OptimizeReferences>
+      <ModuleDefinitionFile>src/IntelQuickSyncDecoder.def</ModuleDefinitionFile>
+      <IgnoreAllDefaultLibraries>false</IgnoreAllDefaultLibraries>
+      <OutputFile>$(SolutionDir)bin\$(TargetName)$(TargetExt)</OutputFile>
+      <ProgramDatabaseFile>$(OutDir)$(TargetName).pdb</ProgramDatabaseFile>
       <AdditionalDependencies>winmm.lib;uuid.lib;D3D9.lib;Dxva2.lib;libmfx.lib;strmiids.lib</AdditionalDependencies>
+      <Version>
+      </Version>
+      <IgnoreSpecificDefaultLibraries>LIBCMTD.LIB</IgnoreSpecificDefaultLibraries>
+      <GenerateDebugInformation>true</GenerateDebugInformation>
+      <LinkTimeCodeGeneration>UseLinkTimeCodeGeneration</LinkTimeCodeGeneration>
     </Link>
-    <ResourceCompile>
-      <Culture>0x0409</Culture>
-    </ResourceCompile>
   </ItemDefinitionGroup>
   <ItemGroup>
     <None Include="src\IntelQuickSyncDecoder.def" />

src/codecs/IntelQuickSyncDecoder/IntelQuickSyncDecoder.vcxproj.filters
@@ -12,9 +12,6 @@
     <Filter Include="Resource Files">
       <UniqueIdentifier>{72d0b727-3294-44aa-9d82-62b45ffad724}</UniqueIdentifier>
     </Filter>
-    <Filter Include="Utility">
-      <UniqueIdentifier>{6b64424a-d463-4ba7-a819-668e8c0d4c3c}</UniqueIdentifier>
-    </Filter>
     <Filter Include="Source Files\Utility">
       <UniqueIdentifier>{db3d63f1-bcc7-45a4-a77e-c82371e4a86b}</UniqueIdentifier>
     </Filter>
@@ -23,6 +20,9 @@
     </Filter>
     <Filter Include="Source Files\Allocators">
       <UniqueIdentifier>{f1fdd9a0-ea0d-4419-b2d6-4fa904080c16}</UniqueIdentifier>
+    </Filter>
+    <Filter Include="Header Files\Utility">
+      <UniqueIdentifier>{d85caa20-ea42-474b-a316-3d0324321b2c}</UniqueIdentifier>
     </Filter>
   </ItemGroup>
   <ItemGroup>
@@ -56,15 +56,6 @@
     <ClInclude Include="src\resource.h">
       <Filter>Header Files</Filter>
     </ClInclude>
-    <ClInclude Include="src\QuickSyncUtils.h">
-      <Filter>Utility</Filter>
-    </ClInclude>
-    <ClInclude Include="src\QsWorkerThread.h">
-      <Filter>Utility</Filter>
-    </ClInclude>
-    <ClInclude Include="src\QsThreadPool.h">
-      <Filter>Utility</Filter>
-    </ClInclude>
     <ClInclude Include="src\base_allocator.h">
       <Filter>Header Files\Allocators</Filter>
     </ClInclude>
@@ -73,6 +64,15 @@
     </ClInclude>
     <ClInclude Include="src\sysmem_allocator.h">
       <Filter>Header Files\Allocators</Filter>
+    </ClInclude>
+    <ClInclude Include="src\QsThreadPool.h">
+      <Filter>Header Files\Utility</Filter>
+    </ClInclude>
+    <ClInclude Include="src\QuickSyncUtils.h">
+      <Filter>Header Files\Utility</Filter>
+    </ClInclude>
+    <ClInclude Include="src\QsWorkerThread.h">
+      <Filter>Header Files\Utility</Filter>
     </ClInclude>
   </ItemGroup>
   <ItemGroup>

src/codecs/TvideoCodecQuickSync.cpp
@@ -28,6 +28,7 @@
 #include "IffdshowBase.h"
 #include "IffdshowDecVideo.h"
 #include "dsutil.h"
+#include "TfakeImediaSample.h"
 #include "IntelQuickSyncDecoder/src/IQuickSyncDecoder.h"
 
 const char_t* TvideoCodecQuickSync::dllname=_l(QS_DEC_DLL_NAME);
@@ -48,6 +49,8 @@ HRESULT TvideoCodecQuickSync::DeliverSurfaceCallback(void* obj, QsFrameData* fra
     return ((TvideoCodecQuickSync*)obj)->DeliverSurface(frameData);
 }
 
+static CMemAllocator g_FakeAllocator("", NULL, NULL);
+
 TvideoCodecQuickSync::TvideoCodecQuickSync(IffdshowBase *Ideci,IdecVideoSink *IsinkD, int codecID) :
     Tcodec(Ideci),
     TcodecDec(Ideci,IsinkD),
@@ -56,7 +59,8 @@ TvideoCodecQuickSync::TvideoCodecQuickSync(IffdshowBase *Ideci,IdecVideoSink *Is
     createQuickSync(NULL),
     destroyQuickSync(NULL),
     m_QuickSync(NULL),
-    m_Dll(NULL)
+    m_Dll(NULL),
+    m_MediaSample(_l("Fake Media Sample"), &g_FakeAllocator, NULL, NULL, 0)
 {
     ok = false;
     m_Dll = new Tdll(dllname,config);
@@ -120,10 +124,24 @@ HRESULT TvideoCodecQuickSync::decompress(const unsigned char *src,size_t srcLen,
     if (!ok) return E_UNEXPECTED;
 
     CAutoLock lock(&m_csLock);
-    bool isSyncPoint = pIn->IsSyncPoint() == S_OK;
-    HRESULT hr = m_QuickSync->Decode(pIn);
 
-    if (pIn->IsPreroll() == S_OK)
+    IMediaSample* pMediaSample = pIn;
+    TfakeMediaSample* pFakeMediaSample = (E_NOTIMPL == pIn->GetActualDataLength()) ? static_cast<TfakeMediaSample*>(pIn) : NULL;
+    
+    // VFW flow
+    if (NULL != pFakeMediaSample)
+    {
+        m_MediaSample.SetDiscontinuity(pFakeMediaSample->IsDiscontinuity() == S_OK ? TRUE : FALSE);
+        m_MediaSample.SetSyncPoint(pFakeMediaSample->IsSyncPoint() == S_OK ? TRUE : FALSE);
+        m_MediaSample.SetPointer(const_cast<BYTE*>(src), srcLen);
+        m_MediaSample.SetActualDataLength(srcLen);
+        pMediaSample = &m_MediaSample;
+    }
+
+    bool isSyncPoint = pMediaSample->IsSyncPoint() == S_OK;
+    HRESULT hr = m_QuickSync->Decode(pMediaSample);
+
+    if (pMediaSample->IsPreroll() == S_OK)
     {
         return sinkD->deliverPreroll((isSyncPoint) ? FRAME_TYPE::I : FRAME_TYPE::P);
     }

src/codecs/TvideoCodecQuickSync.h
@@ -39,6 +39,7 @@ protected:
     CCritSec            m_csLock;
     Tdll*               m_Dll;
     IQuickSyncDecoder*  m_QuickSync;
+    CMediaSample        m_MediaSample;
 };
 
 #endif

src/convert/Tconvert.cpp
@@ -712,64 +712,21 @@ int Tconvert::convert(const TffPict &pict,uint64_t outcsp,uint8_t* dst[],stride_
                    vram_indirect);
 }
 
-void* sse_memcpy(void* d, const void* s, size_t _size)
-{
-    if (d == NULL || s == NULL) return NULL;
-
-    //if memory is not aligned, use memcpy
-    bool isAligned = (((size_t)(s) | (size_t)(d)) & 0xF) == 0;
-    if (!isAligned || _size < 64)
-    {
-        return memcpy(d, s, _size);
-    }
-
-    size_t reminder = _size & 63; // copy 64 bytes every loop
-
-    __m128i* pTrg = (__m128i*)d;
-    __m128i* pTrgEnd = pTrg + ((_size-reminder) >> 4);
-    __m128i* pSrc = (__m128i*)s;
-    while (pTrg < pTrgEnd)
-    {
-        _mm_stream_si128(&pTrg[0], pSrc[0]);
-        _mm_stream_si128(&pTrg[1], pSrc[1]);
-        _mm_stream_si128(&pTrg[2], pSrc[2]);
-        _mm_stream_si128(&pTrg[3], pSrc[3]);
-        pSrc += 4;
-        pTrg += 4;
-    }
-
-    if (reminder)
-    {
-        char* ps = (char*)s + _size - reminder;
-        char* pd = (char*)d + _size - reminder;
-        for (size_t i = 0; i < reminder; ++i)
-        {
-            pd[i] = ps[i];
-        }
-    }
-
-    return d;
-}
-
 void Tconvert::copyPlane(BYTE *dstp,stride_t dst_pitch,const BYTE *srcp,stride_t src_pitch,int row_size,int height,bool flip)
 {
-    // get a pointer to the relevant memcpy function
-    void* (*Memcpy)(void* d, const void* s, size_t) = (Tconfig::cpu_flags&FF_CPU_SSE2) ?
-        sse_memcpy : memcpy;
-
     if (dst_pitch == src_pitch && src_pitch == row_size && !flip) {
-        Memcpy(dstp, srcp, src_pitch * height);
+        memcpy(dstp, srcp, src_pitch * height);
     } else {
         if (!flip) {
             for (int y=height; y>0; --y) {
-                Memcpy(dstp, srcp, row_size);
+                memcpy(dstp, srcp, row_size);
                 dstp += dst_pitch;
                 srcp += src_pitch;
             }
         } else {
             dstp += dst_pitch * (height - 1);
             for (int y=height; y>0; --y) {
-                Memcpy(dstp, srcp, row_size);
+                memcpy(dstp, srcp, row_size);
                 dstp -= dst_pitch;
                 srcp += src_pitch;
             }

commit 579990aea74376e1c9657395817b5b4fd634d9a8

Fixed debug assert failure introduced in r4364. Added simple multi-threaded frame copy to the CopyPlane function (Convert.cpp) - uses PPL. Improves performance by a few percent in some cases. Synced to QuickSync Decoder r45 - cleaned version using PPL based frame copy - same performance as before but safer and much less code. Local VC2010/ICL12 QS decoder projects updated.
git-svn-id: https://ffdshow-tryout.svn.sourceforge.net/svnroot/ffdshow-tryout/trunk@4371 3b938f2f-1a1a-0410-8054-a526ea5ff92c

src/codecs/IntelQuickSyncDecoder/IntelQuickSyncDecoder.vcxproj
@@ -159,8 +159,6 @@
     <ClInclude Include="src\base_allocator.h" />
     <ClInclude Include="src\CodecInfo.h" />
     <ClInclude Include="src\H264Nalu.h" />
-    <ClInclude Include="src\QsThreadPool.h" />
-    <ClInclude Include="src\QsWorkerThread.h" />
     <ClInclude Include="src\QuickSyncDecoder.h" />
     <ClInclude Include="src\d3d_allocator.h" />
     <ClInclude Include="src\IQuickSyncDecoder.h" />
@@ -175,8 +173,6 @@
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="src\base_alllocator.cpp" />
-    <ClCompile Include="src\QsThreadPool.cpp" />
-    <ClCompile Include="src\QsWorkerThread.cpp" />
     <ClCompile Include="src\QuickSyncExports.cpp" />
     <ClCompile Include="src\H264Nalu.cpp" />
     <ClCompile Include="src\QuickSyncDecoder.cpp" />

src/codecs/IntelQuickSyncDecoder/IntelQuickSyncDecoder.vcxproj.filters
@@ -65,13 +65,7 @@
     <ClInclude Include="src\sysmem_allocator.h">
       <Filter>Header Files\Allocators</Filter>
     </ClInclude>
-    <ClInclude Include="src\QsThreadPool.h">
-      <Filter>Header Files\Utility</Filter>
-    </ClInclude>
     <ClInclude Include="src\QuickSyncUtils.h">
-      <Filter>Header Files\Utility</Filter>
-    </ClInclude>
-    <ClInclude Include="src\QsWorkerThread.h">
       <Filter>Header Files\Utility</Filter>
     </ClInclude>
   </ItemGroup>
@@ -96,12 +90,6 @@
     </ClCompile>
     <ClCompile Include="src\TimeManager.cpp">
       <Filter>Source Files</Filter>
-    </ClCompile>
-    <ClCompile Include="src\QsThreadPool.cpp">
-      <Filter>Source Files\Utility</Filter>
-    </ClCompile>
-    <ClCompile Include="src\QsWorkerThread.cpp">
-      <Filter>Source Files\Utility</Filter>
     </ClCompile>
     <ClCompile Include="src\QuickSyncUtils.cpp">
       <Filter>Source Files\Utility</Filter>

src/codecs/IntelQuickSyncDecoder/IntelQuickSyncDecoder_icl12.vcxproj
@@ -163,8 +163,6 @@
     <ClInclude Include="src\base_allocator.h" />
     <ClInclude Include="src\CodecInfo.h" />
     <ClInclude Include="src\H264Nalu.h" />
-    <ClInclude Include="src\QsThreadPool.h" />
-    <ClInclude Include="src\QsWorkerThread.h" />
     <ClInclude Include="src\QuickSyncDecoder.h" />
     <ClInclude Include="src\d3d_allocator.h" />
     <ClInclude Include="src\IQuickSyncDecoder.h" />
@@ -179,8 +177,6 @@
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="src\base_alllocator.cpp" />
-    <ClCompile Include="src\QsThreadPool.cpp" />
-    <ClCompile Include="src\QsWorkerThread.cpp" />
     <ClCompile Include="src\QuickSyncExports.cpp" />
     <ClCompile Include="src\H264Nalu.cpp" />
     <ClCompile Include="src\QuickSyncDecoder.cpp" />

src/codecs/IntelQuickSyncDecoder/IntelQuickSyncDecoder_icl12.vcxproj.filters
@@ -68,13 +68,7 @@
     <ClInclude Include="src\sysmem_allocator.h">
       <Filter>Header Files\Allocators</Filter>
     </ClInclude>
-    <ClInclude Include="src\QsThreadPool.h">
-      <Filter>Header Files\Utility</Filter>
-    </ClInclude>
     <ClInclude Include="src\QuickSyncUtils.h">
-      <Filter>Header Files\Utility</Filter>
-    </ClInclude>
-    <ClInclude Include="src\QsWorkerThread.h">
       <Filter>Header Files\Utility</Filter>
     </ClInclude>
   </ItemGroup>
@@ -99,12 +93,6 @@
     </ClCompile>
     <ClCompile Include="src\TimeManager.cpp">
       <Filter>Source Files</Filter>
-    </ClCompile>
-    <ClCompile Include="src\QsThreadPool.cpp">
-      <Filter>Source Files\Utility</Filter>
-    </ClCompile>
-    <ClCompile Include="src\QsWorkerThread.cpp">
-      <Filter>Source Files\Utility</Filter>
     </ClCompile>
     <ClCompile Include="src\QuickSyncUtils.cpp">
       <Filter>Source Files\Utility</Filter>

src/codecs/TvideoCodecQuickSync.cpp
@@ -49,8 +49,6 @@ HRESULT TvideoCodecQuickSync::DeliverSurfaceCallback(void* obj, QsFrameData* fra
     return ((TvideoCodecQuickSync*)obj)->DeliverSurface(frameData);
 }
 
-static CMemAllocator g_FakeAllocator("", NULL, NULL);
-
 TvideoCodecQuickSync::TvideoCodecQuickSync(IffdshowBase *Ideci,IdecVideoSink *IsinkD, int codecID) :
     Tcodec(Ideci),
     TcodecDec(Ideci,IsinkD),
@@ -60,7 +58,8 @@ TvideoCodecQuickSync::TvideoCodecQuickSync(IffdshowBase *Ideci,IdecVideoSink *Is
     destroyQuickSync(NULL),
     m_QuickSync(NULL),
     m_Dll(NULL),
-    m_MediaSample(_l("Fake Media Sample"), &g_FakeAllocator, NULL, NULL, 0)
+    m_FakeAllocator("", NULL, NULL),
+    m_MediaSample(_l("Fake Media Sample"), &m_FakeAllocator, NULL, NULL, 0)
 {
     ok = false;
     m_Dll = new Tdll(dllname,config);

src/codecs/TvideoCodecQuickSync.h
@@ -39,6 +39,7 @@ protected:
     CCritSec            m_csLock;
     Tdll*               m_Dll;
     IQuickSyncDecoder*  m_QuickSync;
+    CMemAllocator       m_FakeAllocator;
     CMediaSample        m_MediaSample;
 };
 

src/convert/Tconvert.cpp
@@ -712,10 +712,40 @@ int Tconvert::convert(const TffPict &pict,uint64_t outcsp,uint8_t* dst[],stride_
                    vram_indirect);
 }
 
+#define MIN_BUFF_SIZE (1 << 18)
+typedef void* (*Tmemcpy)(void*, const void*, size_t);
+
+static void* mt_copy(void* d, const void* s, size_t size, Tmemcpy memcpyFunc)
+{
+    if (!d || !s) return NULL;
+
+    // Buffer is very small and not worth the effort
+    if (size < MIN_BUFF_SIZE)
+    {
+        return memcpyFunc(d, s, size);
+    }
+
+    size_t blockSize = (size / 2) & ~0xf; // Make size a multiple of 16 bytes
+    std::array<size_t, 3> offsets = { 0, blockSize, size };
+    std::array<size_t, 2> indexes = { 0, 1 };
+
+    Concurrency::parallel_for_each(indexes.begin(), indexes.end(), [d, s, offsets, memcpyFunc](size_t& i)
+    {
+        memcpyFunc((char*)d + offsets[i], (const char*)s + offsets[i], offsets[i + 1] - offsets[i]);
+    });
+
+    return d;
+}
+
+inline void* mt_memcpy(void* d, const void* s, size_t size)
+{
+    return mt_copy(d, s, size, memcpy);
+}
+
 void Tconvert::copyPlane(BYTE *dstp,stride_t dst_pitch,const BYTE *srcp,stride_t src_pitch,int row_size,int height,bool flip)
 {
     if (dst_pitch == src_pitch && src_pitch == row_size && !flip) {
-        memcpy(dstp, srcp, src_pitch * height);
+        mt_memcpy(dstp, srcp, src_pitch * height);
     } else {
         if (!flip) {
             for (int y=height; y>0; --y) {

src/stdafx.h
@@ -50,6 +50,7 @@
  #include <utility>
  #include <limits>
  #include <deque>
+ #include <array>
 #include "tuple.h"
 // baseclasses
 #include <streams.h>
@@ -64,6 +65,7 @@
 #include <qnetwork.h>
 #include <d3d9.h>
 #include <vmr9.h>
+#include <ppl.h>
 #include "msacmdrv.h"
 
 // BOOST